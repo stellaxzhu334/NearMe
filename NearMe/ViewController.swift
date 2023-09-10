5b91342c0938d5eb68a92fb43ef003c9ae794452
Stella <stellazhu334@gmail.com>
2023-09-10 18:23:22 -0400

    Initial Commit

---
+++ NearMe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NearMe
+//
+//  Created by Stella Zhu on 2023/9/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
