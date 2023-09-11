0970593f80f760151d92e62d855ab52de7400f0f
Stella <stellazhu334@gmail.com>
2023-09-11 01:36:26 -0400

    Add address info, direction button and call button on detail view page

---
+++ NearMe/Controllers/PlaceDetailViewController.swift
@@ -0,0 +1,104 @@
+//
+//  PlaceDetailViewController.swift
+//  NearMe
+//
+//  Created by Stella Zhu on 2023/9/11.
+//
+
+import Foundation
+import UIKit
+
+class placeDetailViewController: UIViewController {
+    
+    let place: PlaceAnnotation
+    
+    lazy var nameLabel: UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.lineBreakMode = .byWordWrapping
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .left
+        label.font = UIFont.preferredFont(forTextStyle: .title1)
+        return label
+    } ()
+    
+    lazy var addressLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .left
+        label.font = UIFont.preferredFont(forTextStyle: .body)
+        label.alpha = 0.4
+        return label
+    } ()
+    
+    var directionsButton: UIButton = {
+        var config = UIButton.Configuration.bordered()
+        let button = UIButton(configuration: config)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Directions", for: .normal)
+        return button
+    } ()
+    
+    var callButton: UIButton = {
+        var config = UIButton.Configuration.bordered()
+        let button = UIButton(configuration: config)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Call", for: .normal)
+        return button
+    } ()
+    
+    init(place: PlaceAnnotation) {
+        self.place = place
+        super.init(nibName: nil, bundle: nil)
+        setupUI()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented") // needed for storyboard, no need to care now
+    }
+    
+//    @objc func directionsButtonTapped(_ sender: UIButton) {
+//        let coordinate  = place.location.coordinate
+//        guard let url = URL(string: "http://maps.apple.com/?daddr=\(coordinate.latitude),\(coordinate.longitude)") else { return }
+//
+//        UIApplication.shared.open(url)
+//    }
+    
+    private func setupUI() {
+        
+        let stackView = UIStackView()
+        stackView.alignment = .leading
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = UIStackView.spacingUseSystem
+        stackView.isLayoutMarginsRelativeArrangement = true
+        stackView.directionalLayoutMargins = NSDirectionalEdgeInsets(top: 20, leading: 20, bottom: 20, trailing: 20)
+        
+        nameLabel.text = place.name
+        addressLabel.text = place.address
+        
+        stackView.addArrangedSubview(nameLabel)
+        stackView.addArrangedSubview(addressLabel)
+        
+        nameLabel.widthAnchor.constraint(equalToConstant: view.bounds.width - 20).isActive = true
+        
+        let contactStackView = UIStackView()  // for adding two buttons horizontally
+        contactStackView.translatesAutoresizingMaskIntoConstraints = false
+        contactStackView.axis = .horizontal
+        contactStackView.spacing = UIStackView.spacingUseSystem
+        
+//        directionsButton.addTarget(self, action: #selector(directionsButtonTapped), for: .touchUpInside)
+        
+        contactStackView.addArrangedSubview(directionsButton)
+        contactStackView.addArrangedSubview(callButton)
+        
+        stackView.addArrangedSubview(contactStackView)
+        
+        view.addSubview(stackView)
+    }
+}
